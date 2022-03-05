0b8f7436fada16240e1e40986dd01be8f00630d2
DariuszMaj <91788770+DariuszMaj@users.noreply.github.com>
2022-03-05 16:01:18 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace firstapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ulamek a = new Ulamek();
+            Ulamek b = new Ulamek(1, 8);
+            Ulamek c=a + b;
+            Console.WriteLine(c);
+            Console.WriteLine(b.ToString());
+
+        }
+    }
+
+  
+    
+  
+
+
+    class Ulamek 
+    {
+        
+
+
+
+
+    private int licznik;
+        private int mianownik;
+
+        public Ulamek()
+        {
+            this.licznik = 1;
+            this.mianownik = 1;
+        }
+
+
+        public Ulamek(int licznik, int mianownik)
+        {
+            this.licznik = licznik;
+            this.mianownik = mianownik;
+        }
+        public Ulamek(Ulamek copy)
+        {
+
+            licznik = copy.licznik;
+            mianownik = copy.mianownik;
+
+        }
+        public Ulamek()
+        {
+
+        }
+
+        public override string ToString() => $"Licznik: {licznik} Mianownik: {mianownik}";
+
+
+    }
+
+  
+
+    
+    
+}
