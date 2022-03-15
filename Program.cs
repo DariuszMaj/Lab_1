5751e0bbeff94e06620bbc40b59d0c6c02863ea6
DariuszMaj <91788770+DariuszMaj@users.noreply.github.com>
2022-03-15 20:30:34 +0100

    Add files via upload
    update main

--- Program.cs
+++ Program.cs
@@ -1,67 +1,142 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace firstapp
+using System.IO;
+
+namespace Lab_01PO
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
-
-            Ulamek a = new Ulamek();
-            Ulamek b = new Ulamek(1, 8);
-            Ulamek c=a + b;
+            Ulamek a = new Ulamek(1, 0);
+            Ulamek b = new Ulamek(-1, 2);
+            Ulamek c = a + b;
+            Ulamek d = a - b;
+            Ulamek e = a / b;
+            Ulamek f = a * b;
             Console.WriteLine(c);
-            Console.WriteLine(b.ToString());
-
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(Ulamek.RoundValueUp(a));
+           
         }
     }
-
-  
-    
-  
-
-
-    class Ulamek 
+    public class Ulamek : IComparable<Ulamek>
     {
-        
+
+        public int CompareTo(Ulamek a)
+        {
+            var first = ((double)a.licznik / (double)a.mianownik);
+            var second = ((double)this.licznik / (double)this.mianownik);
+            if (second.Equals(a)) return 0;
+            else if (first > second) return 1;
+            else return -1;
+
+        }
+
+
+        //1. PRYWATNE ZMIENNE
+        public int licznik;
+       
+       public int mianownik;
 
 
 
-
-    private int licznik;
-        private int mianownik;
-
+        //KONSTRUKTOR DOMYŚLNY
         public Ulamek()
         {
             this.licznik = 1;
             this.mianownik = 1;
         }
 
-
+        //KONSTRUKTOR Z DWOMA ARGUMENTAMI
         public Ulamek(int licznik, int mianownik)
         {
-            this.licznik = licznik;
-            this.mianownik = mianownik;
+            if (mianownik == 0) throw new ArgumentException("Wrong details!");
+            else if (licznik == 0)
+            {
+                licznik = 0;
+                mianownik = 0;
+            }
+            else
+            {
+                this.licznik = licznik;
+                this.mianownik = mianownik;
+            }
         }
+
+        //KONSTRUKTOR KOPIUJĄCY
         public Ulamek(Ulamek copy)
         {
-
             licznik = copy.licznik;
             mianownik = copy.mianownik;
-
         }
-        public Ulamek()
+
+
+        //PRZEŁADOWANIE DODAWANIA
+        public static Ulamek operator +(Ulamek a, Ulamek b)
+
         {
 
+            var c = (a.mianownik) * (b.mianownik);
+            var d = (a.licznik) * b.mianownik;
+            var e = (b.licznik) * a.mianownik;
+            var up = d + e;
+            if (up == 0) return new Ulamek(0, c);
+
+            return new Ulamek(up, c);
         }
 
+        //PRZEŁADOWANIE ODEJMOWANIA
+        public static Ulamek operator -(Ulamek a, Ulamek b)
+        {
+            var c = (a.mianownik) * (b.mianownik);
+            var d = (a.licznik) * b.mianownik;
+            var e = (b.licznik) * a.mianownik;
+            var up = d - e;
+            if (up == 0) return new Ulamek(0,c);
+
+            return new Ulamek(up, c);
+        }
+
+        //PRZEŁADOWANIE DZIELENIA
+        public static Ulamek operator /(Ulamek a, Ulamek b)
+        {
+            var up = a.licznik * b.mianownik;
+            var down = a.mianownik * b.licznik;
+            return new Ulamek(up, down);
+        }
+
+        //PRZEŁADOWANIE MNOŻENIA
+        public static Ulamek operator *(Ulamek a, Ulamek b)
+        {
+            var up = a.licznik * b.licznik;
+            var down = a.mianownik * b.mianownik;
+            return new Ulamek(up, down);
+        }
+        //METODA ZAOKRĄGLANIE DO GÓRY
+        public static int RoundValueUp (Ulamek a)
+        {
+            return (int)Math.Ceiling((double)a.licznik / (double)a.mianownik);
+        }
+
+        //METODA ZAOKRĄGLANIE NA DÓŁ
+        public static int RoundValueDown(Ulamek a)
+        {
+            return (int)Math.Floor((double)a.licznik / (double)a.mianownik);
+        }
+
+        //METODA TO STRING
         public override string ToString() => $"Licznik: {licznik} Mianownik: {mianownik}";
+
+        //  public static fraction operator +(fraction a)=>a;
+        //    public static fraction operator -(fraction a)=>new fraction(-a.licznik,a.mianownik);
+
 
 
     }
 
-  
-
-    
-    
 }
