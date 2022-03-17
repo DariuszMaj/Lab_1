60328dd8eb1f4f9694d0a5411f00d819200ae2a5
DariuszMaj <91788770+DariuszMaj@users.noreply.github.com>
2022-03-17 17:28:23 +0100

    Add files via upload
    updated unit test

--- Ulamek_test.cs
+++ Ulamek_test.cs
@@ -27,7 +27,7 @@ namespace Ulamki_TEST
             var b = new Ulamek(-1, 2);
             var c = a + b;   
             Assert.AreEqual(0,c.licznik);
-            Assert.AreEqual(4,c.mianownik);
+            Assert.AreEqual(0,c.mianownik);
         }
 
          [Test]
@@ -37,8 +37,8 @@ namespace Ulamki_TEST
             var a = new Ulamek(1, 8);
             var b = new Ulamek(-1, -32);
             var c = a + b;   
-            Assert.AreEqual(-40,c.licznik);
-            Assert.AreEqual(-256,c.mianownik);
+            Assert.AreEqual(40,c.licznik);
+            Assert.AreEqual(256,c.mianownik);
         }
         [Test]
 
@@ -77,25 +77,31 @@ namespace Ulamki_TEST
             Assert.AreEqual(8, c.mianownik);
         }
 
+        [Test]
+
+        public void Odejmowanie_Liczby_Z_Ujemnym_Licznikiem_Mianownikiem()
+        {
+            var a = new Ulamek(24, 18);
+            var b = new Ulamek(-37, -72);
+            var c = a - b;
+            Assert.AreEqual(1062, c.licznik);
+            Assert.AreEqual(1296, c.mianownik);
+        }
+        [Test]
+
+        public void Odejmowanie_Liczb_ujemnych()
+        {
+            var a = new Ulamek(-11, -22);
+            var b = new Ulamek(-169, -1232);
+            var c = a - b;
+            Assert.AreEqual(9834, c.licznik);
+            Assert.AreEqual(27104, c.mianownik);
+        }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //DZIELENIE
 
 
         [Test]
@@ -107,5 +113,20 @@ namespace Ulamki_TEST
             Assert.AreEqual(128, c.licznik);
             Assert.AreEqual(128, c.mianownik);
         }
+
+        [Test]
+        public void Mnozenie_Liczb()
+        {
+            var a = new Ulamek(64, 256);
+            var b = new Ulamek(1024, 512);
+            var c = a * b;
+            Assert.AreEqual(65536, c.licznik);
+            Assert.AreEqual(131072, c.mianownik);
+        }
+
+
+
+
+
     }
 }
